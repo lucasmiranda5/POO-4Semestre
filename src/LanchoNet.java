1d0cbdd164b6ce623af66321ff98117e07d5f955
Lucas Miranda <lucasmiranda.dw@gmail.com>
2016-06-29 18:37:22 -0300

    primeiro commit

---
+++ src/LanchoNet.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lucasmiranda
+ */
+public class LanchoNet {
+    
+      public static void main(String[] args) {
+       loginAdm t = new loginAdm();
+       Conexao con = new Conexao();
+       t.setVisible(true);
+    }    
+}
